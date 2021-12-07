96f97f3c6243cc6500f18500ba5f21de7024517a
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2021-12-07 14:41:01 +0100

    Create Get_Binance_Data.py

---
+++ Get_Binance_Data.py
@@ -0,0 +1,40 @@
+# https://python-binance.readthedocs.io/en/latest/
+
+import pandas as pd
+from binance.client import Client
+import ta
+
+klinesT = Client().get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "01 December 2021")
+
+df = pd.DataFrame(klinesT,
+                  columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades',
+                           'tb_base_av', 'tb_quote_av', 'ignore'])
+
+# [[
+#    1499040000000,      // Open time
+#    "0.01634790",       // Open
+#    "0.80000000",       // High
+#    "0.01575800",       // Low
+#    "0.01577100",       // Close
+#    "148976.11427815",  // Volume
+#    1499644799999,      // Close time
+#    "2434.19055334",    // Quote asset volume
+#    308,                // Number of trades
+#    "1756.87402397",    // Taker buy base asset volume
+#    "28.46694368",      // Taker buy quote asset volume
+#    "17928899.62484339" // Ignore.
+#  ]]
+
+del df['ignore']
+del df['close_time']
+del df['quote_av']
+del df['trades']
+del df['tb_base_av']
+del df['tb_quote_av']
+
+df['close'] = pd.to_numeric(df['close'])
+df['high'] = pd.to_numeric(df['high'])
+df['low'] = pd.to_numeric(df['low'])
+df['open'] = pd.to_numeric(df['open'])
+
+print(df)
