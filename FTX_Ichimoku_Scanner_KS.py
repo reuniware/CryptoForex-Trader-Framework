7742198f9178b833d1ea1318cfef13f272d9e8f4
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2021-12-13 22:52:32 +0100

    Update FTX_Ichimoku_Scanner_KS.py

--- FTX_Ichimoku_Scanner_KS.py
+++ FTX_Ichimoku_Scanner_KS.py
@@ -30,7 +30,7 @@ for index, row in df.iterrows():
 
     data = client.get_historical_data(
         market_name=symbol,
-        resolution=60 * 60,  # 60min * 60sec = 3600 sec
+        resolution=60 * 60 * 4,  # 60min * 60sec = 3600 sec
         limit=10000,
         start_time=float(round(time.time())) - 150 * 3600,  # 1000*3600 for resolution=3600*24 (daily)
         end_time=float(round(time.time())))
@@ -40,20 +40,27 @@ for index, row in df.iterrows():
     # dframe['time'] = pd.to_datetime(dframe['time'], unit='ms')
 
     # print(dframe)
-    dframe['ICH_SSA'] = ta.trend.ichimoku_a(dframe['high'], dframe['low'])
-    dframe['ICH_SSB'] = ta.trend.ichimoku_b(dframe['high'], dframe['low'])
-    dframe['ICH_KS'] = ta.trend.ichimoku_base_line(dframe['high'], dframe['low'])
-    dframe['ICH_TS'] = ta.trend.ichimoku_conversion_line(dframe['high'], dframe['low'])
+    try:
+        dframe['ICH_SSA'] = ta.trend.ichimoku_a(dframe['high'], dframe['low'])
+        dframe['ICH_SSB'] = ta.trend.ichimoku_b(dframe['high'], dframe['low'])
+        dframe['ICH_KS'] = ta.trend.ichimoku_base_line(dframe['high'], dframe['low'])
+        dframe['ICH_TS'] = ta.trend.ichimoku_conversion_line(dframe['high'], dframe['low'])
+        dframe['ICH_CS'] = dframe['close'].shift(-26)
+
+    except KeyError as err:
+        print(err)
+        continue
 
     for indexdf, rowdf in dframe.iterrows():
-        open = float(rowdf['open'])
+        open = rowdf['open']
         high = rowdf['high']
         low = rowdf['low']
-        close = float(rowdf['close'])
+        close = rowdf['close']
         ssa = rowdf['ICH_SSA']
         ssb = rowdf['ICH_SSB']
-        ks = float(rowdf['ICH_KS'])
+        ks = rowdf['ICH_KS']
         ts = rowdf['ICH_TS']
+        cs = rowdf['ICH_CS']
         timestamp = pd.to_datetime(rowdf['time'], unit='ms')
 
         data_hour = timestamp.hour
@@ -61,7 +68,7 @@ for index, row in df.iterrows():
         data_month = timestamp.month
         data_year = timestamp.year
 
-        now = datetime.now() - timedelta(hours=2)
+        now = datetime.now() - timedelta(hours=8)
         now_hour = now.hour
         now_day = now.day
         now_month = now.month
@@ -69,4 +76,5 @@ for index, row in df.iterrows():
 
         if data_day == now_day and data_month == now_month and data_year == now_year and (data_hour >= now_hour):
             if open < ks < close:
-                print(timestamp, symbol, "O", open, "H", high, "L", low, "C", close, "SSA", ssa, "SSB", ssb, "KS", ks, "TS", ts)
+                print(timestamp, symbol, "O", open, "H", high, "L", low, "C", close, "SSA", ssa, "SSB", ssb, "KS", ks, "TS", ts, "CS", cs)
+
