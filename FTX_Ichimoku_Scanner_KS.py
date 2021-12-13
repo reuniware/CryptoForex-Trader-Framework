64c2ae5d962b50e1e69ec879acf63610fb4c517a
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2021-12-13 22:11:27 +0100

    Update FTX_Ichimoku_Scanner_KS.py

--- FTX_Ichimoku_Scanner_KS.py
+++ FTX_Ichimoku_Scanner_KS.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from datetime import timedelta
 
 import ftx
 import pandas as pd
@@ -25,17 +26,18 @@ df.set_index('name')
 for index, row in df.iterrows():
     symbol = row['name']
     # print(symbol)
+    # print("scanning", symbol)
 
     data = client.get_historical_data(
         market_name=symbol,
-        resolution=3600*24,    # 60min * 60sec = 3600 sec ; 3600*24 for scanning in daily timeframe
-        limit=10000,                                        # Can be optimized according to the scanned timeframe
-        start_time=float(round(time.time())) - 1000*3600,   # Can be optimized according to the scanned timeframe
+        resolution=60 * 60,  # 60min * 60sec = 3600 sec
+        limit=10000,
+        start_time=float(round(time.time())) - 150 * 3600,  # 1000*3600 for resolution=3600*24 (daily)
         end_time=float(round(time.time())))
 
     dframe = pd.DataFrame(data)
 
-    dframe['time'] = pd.to_datetime(dframe['time'], unit='ms')
+    # dframe['time'] = pd.to_datetime(dframe['time'], unit='ms')
 
     # print(dframe)
     dframe['ICH_SSA'] = ta.trend.ichimoku_a(dframe['high'], dframe['low'])
@@ -45,13 +47,26 @@ for index, row in df.iterrows():
 
     for indexdf, rowdf in dframe.iterrows():
         open = float(rowdf['open'])
+        high = rowdf['high']
         low = rowdf['low']
         close = float(rowdf['close'])
         ssa = rowdf['ICH_SSA']
         ssb = rowdf['ICH_SSB']
         ks = float(rowdf['ICH_KS'])
         ts = rowdf['ICH_TS']
-        timestamp = rowdf['time']
+        timestamp = pd.to_datetime(rowdf['time'], unit='ms')
 
-        if open < ks < close:
-            print(timestamp, symbol, "O", open, "L", low, "C", close, "SSA", ssa, "SSB", ssb, "KS", ks, "TS", ts)
+        data_hour = timestamp.hour
+        data_day = timestamp.day
+        data_month = timestamp.month
+        data_year = timestamp.year
+
+        now = datetime.now() - timedelta(hours=2)
+        now_hour = now.hour
+        now_day = now.day
+        now_month = now.month
+        now_year = now.year
+
+        if data_day == now_day and data_month == now_month and data_year == now_year and (data_hour >= now_hour):
+            if open < ks < close:
+                print(timestamp, symbol, "O", open, "H", high, "L", low, "C", close, "SSA", ssa, "SSB", ssb, "KS", ks, "TS", ts)
