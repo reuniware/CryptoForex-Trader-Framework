281c6d0cf4702b414e5e196e025d83bf887cf9be
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2021-12-22 12:55:09 +0100

    Update FTX_Ichimoku_Scanner_With_Chikou.py

--- FTX_Ichimoku_Scanner_With_Chikou.py
+++ FTX_Ichimoku_Scanner_With_Chikou.py
@@ -221,41 +221,41 @@ def my_thread(name):
                         # if openp < ssb < close or openp > ssb and close > ssb:
                         # if openp > ssb and close > ssb and close > openp and openp > ssa and close > ssa and openp > ks and openp > ts and close > ks and close > ts:
                         if close / openp > 1 and openp > ssb and close > ssb and openp > ssa and close > ssa:
-                            csresults = ""
+                            cs_results = ""
                             if cs > ssbchikou:
-                                csresults += "* CS > SSBCHIKOU - "
+                                cs_results += "* CS > SSBCHIKOU - "
                             if cs > ssachikou:
-                                csresults += "* CS > SSACHIKOU - "
+                                cs_results += "* CS > SSACHIKOU - "
                             if cs > kijunchikou:
-                                csresults += "* CS > KSCHIKOU - "
+                                cs_results += "* CS > KSCHIKOU - "
                             if cs > tenkanchikou:
-                                csresults += "* CS > TSCHIKOU - "
+                                cs_results += "* CS > TSCHIKOU - "
                             if cs > closechikou:
-                                csresults += "* CS > CLOSECHIKOU"
-                            if csresults != "":
-                                log_to_results(csresults)
+                                cs_results += "* CS > CLOSECHIKOU"
+                            if cs_results != "":
+                                log_to_results(cs_results)
 
                             # print(timestamp, symbol, "O", openp, "H", high, "L", low, "C", close, "SSA", ssa, "SSB", ssb, "KS", ks, "TS", ts, "CS", cs, "EVOL%", evol)
                             # print("")
-                            strn = str(timestamp) + " " + symbol + " " + symbol_type + " SSA=" + str(ssa) + " SSB=" + str(
+                            str_result = str(timestamp) + " " + symbol + " " + symbol_type + " SSA=" + str(ssa) + " SSB=" + str(
                                 ssb) + " KS=" + str(ks) + " TS=" + str(ts) + " O=" + str(openp) + " H=" + str(high) + " L=" + str(
                                 low)  # + " C=" + str(close) + " CS=" + str(cs) + " EVOL%=" + str(evol)
 
-                            if not (strn in list_results):
+                            if not (str_result in list_results):
                                 if not new_results_found:
                                     new_results_found = True
                                 results_count = results_count + 1
-                                list_results.append(strn)
-                                print(csresults)
-                                print(str(results_count) + " " + strn + " C=" + str(close) + " CS=" + str(cs) + " EVOL%=" + str(evol) + "\n")
-                                log_to_results(strn + " C=" + str(close) + " CS=" + str(cs) + " EVOL%(t0)=" + str(evol) + '\n')
+                                list_results.append(str_result)
+                                print(cs_results)
+                                print(str(results_count) + " " + str_result + " C=" + str(close) + " CS=" + str(cs) + " EVOL%=" + str(evol) + "\n")
+                                log_to_results(str_result + " C=" + str(close) + " CS=" + str(cs) + " EVOL%(t0)=" + str(evol) + '\n')
 
                 else:
                     if result_ok:
                         print(timestamp, symbol, "O", openp, "H", high, "L", low, "C", close, "SSA", ssa, "SSB", ssb, "KS", ks, "TS", ts, "CS", cs)
-                        strn = str(timestamp) + " " + symbol + " O=" + str(openp) + " H=" + str(high) + " L=" + str(low) + " C=" + str(close) + " SSA=" + str(ssa) + " SSB=" + str(
+                        str_result = str(timestamp) + " " + symbol + " O=" + str(openp) + " H=" + str(high) + " L=" + str(low) + " C=" + str(close) + " SSA=" + str(ssa) + " SSB=" + str(
                             ssb) + " KS=" + str(ks) + " TS=" + str(ts) + " CS=" + str(cs) + " EVOL%(t0)" + str(evol)
-                        log_to_results(strn)
+                        log_to_results(str_result)
 
         if new_results_found:
             log_to_results(100 * '*' + '\n')
