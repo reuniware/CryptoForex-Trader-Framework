fa503f4210f90ba880e54e2af703360c7f51f8fd
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-06-09 15:18:49 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip install ftx
+pip install pandas
+pip install python-binance
+pip install tradingview_ta
+pip3 install --user --upgrade -e git+https://github.com/twintproject/twint.git@origin/master#egg=twint;
+pip install ta
