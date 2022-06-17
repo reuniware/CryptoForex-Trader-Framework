9cee48f9bc2197f857396aeff98218b051fd598f
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-06-17 12:00:32 +0200

    Create take_profit_calculator.py

---
+++ take_profit_calculator.py
@@ -0,0 +1,8 @@
+# Given a high price and a low price, calculate the high price + 1/4 of the difference between high and low
+
+phigh = 21388
+plow = 20637
+
+tp_level = ((phigh - plow)/4) + phigh
+
+print("tp level =", str(tp_level))
