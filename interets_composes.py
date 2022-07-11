b5a55d6c7df86cb8f2a51efe76fa2f2342dfda49
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-07-12 01:09:13 +0200

    Update interets_composes.py

--- interets_composes.py
+++ interets_composes.py
@@ -1,10 +1,11 @@
-initialbalance = 98.01
+
+initialbalance = 97.15
 finalbalance = initialbalance
 for x in range(365):
     balancebefore = finalbalance
-    finalbalance = finalbalance + (finalbalance/100) * 2
+    finalbalance = finalbalance + (finalbalance/100) * 0.5
     balanceafter = finalbalance
-    print(x, "finalbalance=", finalbalance, "must win was=", balanceafter-balancebefore)
+    print("j+", (x+1), "finalbalance=", finalbalance, "must win was=", balanceafter-balancebefore)
 
 print("finalbalance=", finalbalance)
 
