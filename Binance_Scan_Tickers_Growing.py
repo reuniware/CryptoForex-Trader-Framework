e0be75094d9b596d14c5e61e7f7f6ab606f3cedd
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-08-22 11:20:39 +0200

    Update Binance_Scan_Tickers_Growing.py

--- Binance_Scan_Tickers_Growing.py
+++ Binance_Scan_Tickers_Growing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import ccxt
 import time
 
@@ -24,6 +26,7 @@ percent = 0.5
 array_watch = {}
 array_count = {}
 array_t0 = {}
+array_datetime = {}
 time.sleep(0.1)
 tickers = exchange.fetch_tickers()
 for item in tickers.items():
@@ -34,6 +37,7 @@ for item in tickers.items():
     if ask > 0:
         array_watch[symbol] = ask + ask/100*percent
         array_t0[symbol] = ask
+        array_datetime[symbol] = datetime.now()
         print("adding", symbol, "with target buy price", array_watch[symbol], "current price being", ask)
         array_count[symbol] = 0
 
@@ -49,10 +53,13 @@ while True:
                 if ask >= value_to_watch:
                     array_count[symbol] = array_count[symbol] + 1
                     array_watch[symbol_to_watch] = ask + ask/100*percent
+
+                    timedelta = datetime.now() - array_datetime[symbol]
                     # evol_pourcent = ask / array_t0[symbol]
                     evol_pourcent = ((ask - array_t0[symbol]) / array_t0[symbol]) * 100
+                    percent_per_second = evol_pourcent / timedelta.total_seconds()
                     str_lien = "https://tradingview.com/chart/?symbol=BINANCE%3A" + symbol.replace('/', '')
-                    print(array_count[symbol_to_watch], symbol_to_watch, "is greater or equals to", value_to_watch, "increasing value to watch for", symbol, "to", array_watch[symbol_to_watch], str_lien, "evol/t0", "{:.2f}".format(evol_pourcent) + "%")
+                    print(array_count[symbol_to_watch], symbol_to_watch, "is greater or equals to", value_to_watch, "increasing value to watch for", symbol, "to", array_watch[symbol_to_watch], str_lien, "evol/t0", "{:.2f}".format(evol_pourcent) + "%", "diff(t-t0)", timedelta, "%/sec", "{:.2f}".format(percent_per_second, 4))
                     #beep.beep(1)
 
 exit(-3)
