4e3c09d279c9373e815af8a2627ccfd0804295dc
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-08-22 11:06:40 +0200

    Update Binance_Scan_Tickers_Growing.py

--- Binance_Scan_Tickers_Growing.py
+++ Binance_Scan_Tickers_Growing.py
@@ -1,5 +1,5 @@
 import ccxt
-import beepy as beep
+import time
 
 print('CCXT Version:', ccxt.__version__)
 
@@ -17,10 +17,14 @@ exchange.set_sandbox_mode(False)  # comment if you're not using the testnet
 markets = exchange.load_markets()
 exchange.verbose = False  # debug output
 
+percent = 0.5
+
 #array_watch = {"VET/USDT": 0.02749, "BTC/USDT": 23000, "BAT/USDT": 0.4139}
 #array_watch = {"VET/USDT": 0.02749, "BTC/USDT": 23000, "BAT/USDT": 0.4139}
 array_watch = {}
 array_count = {}
+array_t0 = {}
+time.sleep(0.1)
 tickers = exchange.fetch_tickers()
 for item in tickers.items():
     symbol = item[0]
@@ -28,7 +32,8 @@ for item in tickers.items():
     bid = tickers[symbol]['bid']  # prix de vente (sell)
     ask = tickers[symbol]['ask']  # prix d'achat (buy)
     if ask > 0:
-        array_watch[symbol] = ask + ask/100*0.1
+        array_watch[symbol] = ask + ask/100*percent
+        array_t0[symbol] = ask
         print("adding", symbol, "with target buy price", array_watch[symbol], "current price being", ask)
         array_count[symbol] = 0
 
@@ -43,9 +48,12 @@ while True:
                 ask = tickers[symbol]['ask'] # prix d'achat (buy)
                 if ask >= value_to_watch:
                     array_count[symbol] = array_count[symbol] + 1
-                    array_watch[symbol_to_watch] = ask + ask/100*0.1
-                    print(array_count[symbol_to_watch], symbol_to_watch, "is greater or equals to", value_to_watch, "increasing value to watch for", symbol, "to", array_watch[symbol_to_watch])
-                    beep.beep(1)
+                    array_watch[symbol_to_watch] = ask + ask/100*percent
+                    # evol_pourcent = ask / array_t0[symbol]
+                    evol_pourcent = ((ask - array_t0[symbol]) / array_t0[symbol]) * 100
+                    str_lien = "https://tradingview.com/chart/?symbol=BINANCE%3A" + symbol.replace('/', '')
+                    print(array_count[symbol_to_watch], symbol_to_watch, "is greater or equals to", value_to_watch, "increasing value to watch for", symbol, "to", array_watch[symbol_to_watch], str_lien, "evol/t0", "{:.2f}".format(evol_pourcent) + "%")
+                    #beep.beep(1)
 
 exit(-3)
 
