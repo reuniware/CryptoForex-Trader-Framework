52133161c50a5894e212ef258ec387683730b8d3
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-09-21 15:38:22 +0200

    Update Ichimoku2022.py

--- CCXT_ICHIMOKU/Ichimoku2022.py
+++ CCXT_ICHIMOKU/Ichimoku2022.py
@@ -11,6 +11,9 @@ pd.set_option('display.expand_frame_repr', True)
 
 exchange_binance = ccxt.binance()
 
+# for tf in exchange_binance.timeframes:
+#     print(tf)
+
 # binance.timeframes
 # {'1m': '1m', '3m': '3m', '5m': '5m', '15m': '15m', '30m': '30m', '1h': '1h', '2h': '2h', '4h': '4h', '6h': '6h', '8h': '8h', '12h': '12h',
 #  '1d': '1d', '3d': '3d', '1w': '1w', '1M': '1M'}
@@ -21,74 +24,82 @@ markets = exchange_binance.fetch_markets()
 # print(markets)
 for oneline in markets:
     symbol = oneline['id']
-    if symbol == symbol: #'BTCUSDT':
-        result = exchange_binance.fetch_ohlcv(symbol, '15m', limit=1000)
-        # print(symbol, result)
-        dframe = pd.DataFrame(result)
-        # print(dframe[0])  # UTC timestamp in milliseconds, integer
-        # print(dframe[1])
-        # print(dframe[2])
-        # print(dframe[3])
-        # print(dframe[4])
+    if symbol.endswith("USDT"):  # == symbol: #'BTCUSDT':
 
-        dframe['timestamp'] = pd.to_numeric(dframe[0])
-        dframe['open'] = pd.to_numeric(dframe[1])
-        dframe['high'] = pd.to_numeric(dframe[2])
-        dframe['low'] = pd.to_numeric(dframe[3])
-        dframe['close'] = pd.to_numeric(dframe[4])
+        for tf in exchange_binance.timeframes:
 
-        dframe['ICH_SSB'] = ta.trend.ichimoku_b(dframe['high'], dframe['low'], window2=26, window3=52).shift(26)
-        # print(dframe['ICH_SSB'])
+            try:
 
-        dframe['ICH_SSA'] = ta.trend.ichimoku_a(dframe['high'], dframe['low'], window1=9, window2=26).shift(26)
-        # print(dframe['ICH_SSA'])
+                result = exchange_binance.fetch_ohlcv(symbol, tf, limit=52)
+                print(tf, symbol, result)
+                dframe = pd.DataFrame(result)
+                # print(dframe[0])  # UTC timestamp in milliseconds, integer
+                # print(dframe[1])
+                # print(dframe[2])
+                # print(dframe[3])
+                # print(dframe[4])
 
-        dframe['ICH_KS'] = ta.trend.ichimoku_base_line(dframe['high'], dframe['low'])
-        # print(dframe['ICH_KS'])
+                dframe['timestamp'] = pd.to_numeric(dframe[0])
+                dframe['open'] = pd.to_numeric(dframe[1])
+                dframe['high'] = pd.to_numeric(dframe[2])
+                dframe['low'] = pd.to_numeric(dframe[3])
+                dframe['close'] = pd.to_numeric(dframe[4])
 
-        dframe['ICH_TS'] = ta.trend.ichimoku_conversion_line(dframe['high'], dframe['low'])
-        # print(dframe['ICH_TS'])
+                dframe['ICH_SSB'] = ta.trend.ichimoku_b(dframe['high'], dframe['low'], window2=26, window3=52).shift(26)
+                # print(dframe['ICH_SSB'])
 
-        dframe['ICH_CS'] = dframe['close'].shift(-26)
-        # print(dframe['ICH_CS'])
+                dframe['ICH_SSA'] = ta.trend.ichimoku_a(dframe['high'], dframe['low'], window1=9, window2=26).shift(26)
+                # print(dframe['ICH_SSA'])
 
-        ssb = dframe['ICH_SSB'].iloc[-1]
-        ssa = dframe['ICH_SSA'].iloc[-1]
-        kijun = dframe['ICH_KS'].iloc[-1]
-        tenkan = dframe['ICH_TS'].iloc[-1]
-        chikou = dframe['ICH_CS'].iloc[-27]
-        # print("SSB", ssb)  # SSB at the current price
-        # print("SSA", ssa)  # SSB at the current price
-        # print("KS", kijun)  # SSB at the current price
-        # print("TS", tenkan)  # SSB at the current price
-        # print("CS", chikou)  # SSB at the current price
+                dframe['ICH_KS'] = ta.trend.ichimoku_base_line(dframe['high'], dframe['low'])
+                # print(dframe['ICH_KS'])
 
-        price_open = dframe['open'].iloc[-1]
-        price_high = dframe['high'].iloc[-1]
-        price_low = dframe['low'].iloc[-1]
-        price_close = dframe['close'].iloc[-1]
-        # print("price_open", price_open)
-        # print("price_high", price_high)
-        # print("price_low", price_low)
-        # print("price_close", price_close)
+                dframe['ICH_TS'] = ta.trend.ichimoku_conversion_line(dframe['high'], dframe['low'])
+                # print(dframe['ICH_TS'])
 
-        price_open_chikou = dframe['open'].iloc[-27]
-        price_high_chikou = dframe['high'].iloc[-27]
-        price_low_chikou = dframe['low'].iloc[-27]
-        price_close_chikou = dframe['close'].iloc[-27]
-        # print("price_open_chikou", price_open_chikou)
-        # print("price_high_chikou", price_high_chikou)
-        # print("price_low_chikou", price_low_chikou)
-        # print("price_close_chikou", price_close_chikou)
+                dframe['ICH_CS'] = dframe['close'].shift(-26)
+                # print(dframe['ICH_CS'])
 
-        tenkan_chikou = dframe['ICH_TS'].iloc[-27]
-        kijun_chikou = dframe['ICH_KS'].iloc[-27]
-        ssa_ichimoku = dframe['ICH_SSA'].iloc[-27]
-        ssb_ichimoku = dframe['ICH_SSB'].iloc[-27]
-        # print("tenkan_chikou", tenkan_chikou)
-        # print("kijun_chikou", kijun_chikou)
-        # print("ssa_ichimoku", ssa_ichimoku)
-        # print("ssb_ichimoku", ssb_ichimoku)
+                ssb = dframe['ICH_SSB'].iloc[-1]
+                ssa = dframe['ICH_SSA'].iloc[-1]
+                kijun = dframe['ICH_KS'].iloc[-1]
+                tenkan = dframe['ICH_TS'].iloc[-1]
+                chikou = dframe['ICH_CS'].iloc[-27]
+                # print("SSB", ssb)  # SSB at the current price
+                # print("SSA", ssa)  # SSB at the current price
+                # print("KS", kijun)  # SSB at the current price
+                # print("TS", tenkan)  # SSB at the current price
+                # print("CS", chikou)  # SSB at the current price
 
-        if price_close > ssa and price_close > ssb and price_close > tenkan and price_close > kijun:
-            print("symbol ok", symbol)
+                price_open = dframe['open'].iloc[-1]
+                price_high = dframe['high'].iloc[-1]
+                price_low = dframe['low'].iloc[-1]
+                price_close = dframe['close'].iloc[-1]
+                # print("price_open", price_open)
+                # print("price_high", price_high)
+                # print("price_low", price_low)
+                # print("price_close", price_close)
+
+                price_open_chikou = dframe['open'].iloc[-27]
+                price_high_chikou = dframe['high'].iloc[-27]
+                price_low_chikou = dframe['low'].iloc[-27]
+                price_close_chikou = dframe['close'].iloc[-27]
+                # print("price_open_chikou", price_open_chikou)
+                # print("price_high_chikou", price_high_chikou)
+                # print("price_low_chikou", price_low_chikou)
+                # print("price_close_chikou", price_close_chikou)
+
+                tenkan_chikou = dframe['ICH_TS'].iloc[-27]
+                kijun_chikou = dframe['ICH_KS'].iloc[-27]
+                ssa_ichimoku = dframe['ICH_SSA'].iloc[-27]
+                ssb_ichimoku = dframe['ICH_SSB'].iloc[-27]
+                # print("tenkan_chikou", tenkan_chikou)
+                # print("kijun_chikou", kijun_chikou)
+                # print("ssa_ichimoku", ssa_ichimoku)
+                # print("ssb_ichimoku", ssb_ichimoku)
+
+                if price_close > ssa and price_close > ssb and price_close > tenkan and price_close > kijun:
+                    print(tf, "symbol ok ", symbol)
+
+            except:
+                pass
