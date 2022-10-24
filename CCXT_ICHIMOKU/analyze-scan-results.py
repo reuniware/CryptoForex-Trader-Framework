d50db51c616495866249b6b02326039a8670761f
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-10-24 12:56:37 +0200

    Update analyze-scan-results.py

--- CCXT_ICHIMOKU/analyze-scan-results.py
+++ CCXT_ICHIMOKU/analyze-scan-results.py
@@ -18,27 +18,31 @@ exchange = exchanges['binance']
 filetoprocess = "202210231230_scan_binance_usdt_gotk.txt"
 
 for filename in os.listdir("ScanResults"):
-    if filename == filetoprocess:
+    if filename == filename:
         print("processing", filename)
         line = 1
-        with open(os.path.join("ScanResults", filename), 'r') as f:
-            # text = f.read()
-            while True:
-                text = f.readline()
-                if not text:
-                    break
-                if line > 1:
-                    symbol = text.split(' ')[0]
-                    price = float(text.split('[')[2].split('= ')[1].split(']')[0])
-                    result = exchange.fetch_ohlcv(symbol, '1m', limit=1)
-                    currentprice = float(result[0][4])
-                    #evol = float("{:.2f}".format((currentprice - price)/price*100))
-                    evol = (currentprice - price)/price*100
+        try:
+            with open(os.path.join("ScanResults", filename), 'r') as f:
+                # text = f.read()
+                while True:
+                    text = f.readline()
+                    if not text:
+                        break
+                    if line > 1:
+                        symbol = text.split(' ')[0]
+                        price = float(text.split('[')[2].split('= ')[1].split(']')[0])
+                        result = exchange.fetch_ohlcv(symbol, '1m', limit=1)
+                        currentprice = float(result[0][4])
+                        #evol = float("{:.2f}".format((currentprice - price)/price*100))
+                        evol = (currentprice - price)/price*100
 
-                    fill_price = " " * (8-len(str(price)))
-                    fill_currentprice = " " * (8-len(str(currentprice)))
-                    fill_evol = " " * (8-len(str(evol)))
+                        fill_symbol = " " * (8-len(str(symbol)))
+                        fill_price = " " * (8-len(str(price)))
+                        fill_currentprice = " " * (8-len(str(currentprice)))
+                        fill_evol = " " * (8-len(str(evol)))
 
-                    print(symbol, "\t[" + str(price) + "]", fill_price, "\t[" + str(currentprice) + "]", fill_currentprice, "\t[" + "{:.2f}".format(evol) + " %]", fill_evol, "\t[" + text.split('[')[0].split('spot')[2] + "]")
-                line += 1
+                        print(filename, "\t", symbol, fill_symbol, "\t[" + str(price) + "]", fill_price, "\t[" + str(currentprice) + "]", fill_currentprice, "\t[" + "{:.2f}".format(evol) + " %]", fill_evol, "\t[" + text.split('[')[0].split('spot')[2] + "]")
+                    line += 1
             # print(text)
+        except:
+            pass
