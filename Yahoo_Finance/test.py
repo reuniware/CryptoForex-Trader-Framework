e0a48dc7fae15d6d24aa1a4e4297dbca32570752
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-12-15 11:58:25 +0100

    Update test.py

--- Yahoo_Finance/test.py
+++ Yahoo_Finance/test.py
@@ -1,5 +1,10 @@
 from yahoo_fin.stock_info import get_data
+from yahoo_fin.stock_info import get_currencies
+
 
 amazon_weekly= get_data("amzn", start_date="12/04/2009", end_date="12/04/2019", index_as_date = True, interval="1wk")
-
 print(amazon_weekly)
+
+
+a = get_currencies()
+print(a)
