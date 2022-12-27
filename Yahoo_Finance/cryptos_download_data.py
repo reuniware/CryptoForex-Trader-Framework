a0ee8db76b3f7ceb07936f997c32e15c8da464c4
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-12-27 15:14:49 +0100

    Update cryptos_download_data.py

--- Yahoo_Finance/cryptos_download_data.py
+++ Yahoo_Finance/cryptos_download_data.py
@@ -1,9 +1,14 @@
 # import yfinance as yf
 # import requests
+import sys
+import time
+
 from requests_html import HTMLSession
 import pandas as pd
 import numpy as np
 from yahoofinancials import YahooFinancials
+import datetime
+from datetime import timedelta
 
 # crypto_codes = yf.symbols("crypto")
 # print(yf.Tickers("crypto"))
@@ -15,6 +20,18 @@ from yahoofinancials import YahooFinancials
 # btc_df.head()
 # print(btc_df)
 # #exit(55)
+
+today = datetime.date.today()
+year = '{:04d}'.format(today.year)
+month = '{:02d}'.format(today.month)
+day = '{:02d}'.format(today.day)
+end_date_str = year + "-" + month + "-" + day
+start_date = pd.to_datetime(end_date_str, format="%Y-%m-%d") - pd.DateOffset(days=1)
+year = '{:04d}'.format(start_date.year)
+month = '{:02d}'.format(start_date.month)
+day = '{:02d}'.format(start_date.day)
+start_date_str = year + "-" + month + "-" + day
+
 
 session = HTMLSession()
 num_currencies = 250
@@ -29,7 +46,7 @@ try:
         # print(df.head(250))
         for symbol in symbols_yf:
             yahoo_financials = YahooFinancials(symbol)
-            data = yahoo_financials.get_historical_price_data("2000-07-10", "2022-12-30", "daily")  # daily weekly monthly
+            data = yahoo_financials.get_historical_price_data(start_date_str, end_date_str, "daily")  # daily weekly monthly
             symbol_df = pd.DataFrame(data[symbol]['prices'])
             symbol_df = symbol_df.drop('date', axis=1).set_index('formatted_date')
             #symbol_df.head()
@@ -38,5 +55,7 @@ try:
             open_price = symbol_df.iloc[-1]['open']
             current_day_evol = (close_price - open_price)/open_price*100
             print("data downloaded for", symbol, current_day_evol, "%")
+            time.sleep(0.25)
 except:
+    print(sys.exc_info())
     print("end of getting cryptos from yf")
