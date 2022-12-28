dd218b68ebcbbd9a9aab51719318746c118dc97f
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2022-12-28 10:51:51 +0100

    Update cryptos_download_data.py

--- Yahoo_Finance/cryptos_download_data.py
+++ Yahoo_Finance/cryptos_download_data.py
@@ -10,6 +10,7 @@ from yahoofinancials import YahooFinancials
 import datetime
 from datetime import timedelta
 import yfinance as yf
+import ta
 
 # crypto_codes = yf.symbols("crypto")
 # print(yf.Tickers("crypto"))
@@ -55,17 +56,44 @@ try:
             close_price = symbol_df.iloc[-1]['close']
             open_price = symbol_df.iloc[-1]['open']
             current_day_evol = (close_price - open_price) / open_price * 100
-            print("data downloaded for", symbol, current_day_evol, "%")
-            # data = yf.download(tickers=symbol, period='10d', interval='15m')
-            # print(data)
-
             dict_evol[symbol] = current_day_evol
+            #print("data downloaded for", symbol, current_day_evol, "%")
 
-            time.sleep(0.25)
+            dframe = yf.download(tickers=symbol, period='56d', interval='1d', progress=False)
+            # # print(data)
+            dframe['ICH_SSB'] = ta.trend.ichimoku_b(dframe['High'], dframe['Low'], window2=26, window3=52).shift(26)
+            # print(dframe['ICH_SSB'])
+            dframe['ICH_SSA'] = ta.trend.ichimoku_a(dframe['High'], dframe['Low'], window1=9, window2=26).shift(26)
+            # print(dframe['ICH_SSA'])
+            dframe['ICH_KS'] = ta.trend.ichimoku_base_line(dframe['High'], dframe['Low'])
+            # print(dframe['ICH_KS'])
+            dframe['ICH_TS'] = ta.trend.ichimoku_conversion_line(dframe['High'], dframe['Low'])
+            # print(dframe['ICH_TS'])
+            dframe['ICH_CS'] = dframe['Close'].shift(-26)
+            # print(dframe['ICH_CS'])
+            ssb = dframe['ICH_SSB'].iloc[-1]
+            ssa = dframe['ICH_SSA'].iloc[-1]
+            kijun = dframe['ICH_KS'].iloc[-1]
+            tenkan = dframe['ICH_TS'].iloc[-1]
+            chikou = dframe['ICH_CS'].iloc[-27]
+            # print("SSB", ssb)  # SSB at the current price
+            # print("SSA", ssa)  # SSB at the current price
+            # print("KS", kijun)  # SSB at the current price
+            # print("TS", tenkan)  # SSB at the current price
+            # print("CS", chikou)  # SSB at the current price
+            price_open = dframe['Open'].iloc[-1]
+            price_high = dframe['High'].iloc[-1]
+            price_low = dframe['Low'].iloc[-1]
+            price_close = dframe['Close'].iloc[-1]
+
+            if price_close > ssb and price_close > ssa and price_close > tenkan and price_close > kijun:
+                print(symbol, "price above ssb and ssa and tenkan and kijun")
+
+            time.sleep(0.25/4)
 
 except:
     print(sys.exc_info())
     print("end of getting cryptos from yf")
 
-for k in sorted(dict_evol, key=lambda k: dict_evol[k]):
+for k in sorted(dict_evol, key=lambda k: dict_evol[k], reverse=True):
     print(k, dict_evol[k])
