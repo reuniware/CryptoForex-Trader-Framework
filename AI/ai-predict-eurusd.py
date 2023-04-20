91c59e1c548e21d7c96a43d8977966ce8fcb857e
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-04-20 15:40:34 +0200

    Update ai-predict-eurusd.py

--- AI/ai-predict-eurusd.py
+++ AI/ai-predict-eurusd.py
@@ -1,6 +1,3 @@
-# ne fonctionne pas (récupération des données historiques + récupération des données temps réel)
-
-import yfinance as yf
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -8,6 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import signal
 import os
+import yfinance as yf
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
@@ -16,13 +14,13 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-# Récupération des données de trading de l'EUR/USD depuis l'API Yahoo
-eurusd_data = yf.download('EURUSD=X', period='2y', interval='1h')
-eurusd_data.reset_index(inplace=True)
+# Récupération des données de trading de EUR/USD depuis l'API Yahoo Finance
+eur_usd_data = yf.download('EURUSD=X', start='2023-01-01', end='2023-04-20', interval='1h')
+eur_usd_data.reset_index(inplace=True)
 
 # Préparation des données pour le modèle LSTM
 scaler = MinMaxScaler(feature_range=(0, 1))
-data = scaler.fit_transform(np.array(eurusd_data['Close']).reshape(-1, 1))
+data = scaler.fit_transform(np.array(eur_usd_data['Close']).reshape(-1, 1))
 training_size = int(len(data) * 0.7)
 test_size = len(data) - training_size
 train_data, test_data = data[0:training_size, :], data[training_size:len(data), :]
@@ -49,23 +47,23 @@ model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1
 model.add(LSTM(units=50))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=None, verbose=1)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1, batch_size=None, verbose=1)
 
 # Sauvegarde du modèle
-model.save('eurusd_lstm_model.h5')
+model.save('eur_usd_lstm_model.h5')
 
 # Utilisation du modèle pour prédire le prix de l'EUR/USD en temps réel
 # n_last_prices doit être égal à time_step
 n_last_prices = 100 # nombre de derniers prix à utiliser pour la prédiction
 while True:
     try:
-        current_price = yf.Ticker('EURUSD=X').info['regularMarketPrice']
-        last_n_prices = scaler.transform(np.array(eurusd_data.tail(n_last_prices)['Close']).reshape(-1, 1))
+        current_price = eur_usd_data['Close'].iloc[-1]
+        last_n_prices = scaler.transform(np.array(eur_usd_data.tail(n_last_prices)['Close']).reshape(-1, 1))
         last_n_prices = np.array(last_n_prices).reshape(1, -1)
         last_n_prices = np.reshape(last_n_prices, (last_n_prices.shape[0], last_n_prices.shape[1], 1))
         predicted_price = model.predict(last_n_prices)
         predicted_price = scaler.inverse_transform(predicted_price)
-        print(f'ia3: Current Price: {current_price:.2f} | Predicted Price: {predicted_price[0][0]:.2f}')
-        eurusd_data.loc[len(eurusd_data)] = [pd.Timestamp.now(), None, None, None, current_price, None]
+        print(f'Current Price: {current_price:.2f} | Predicted Price: {predicted_price[0][0]:.2f}')
+        eur_usd_data.loc[len(eur_usd_data)] = [pd.Timestamp.now(), None, None, None, current_price, None]
     except:
         continue
