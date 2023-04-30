2748ab8e75b49a28afff021b147728fc38e59cef
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-04-30 23:21:43 +0200

    Update ai-predict-bitcoin-14-loop.py

--- AI/AI-BTCUSDT/ai-predict-bitcoin-14-loop.py
+++ AI/AI-BTCUSDT/ai-predict-bitcoin-14-loop.py
@@ -1,5 +1,6 @@
 #!pip install ccxt
 #!pip install python-binance
+#!pip install ta
 
 import ccxt
 import pandas as pd
@@ -57,11 +58,11 @@ if not os.path.exists(directory_modeles_a_trier):
     os.makedirs(directory_modeles_a_trier)
 
 
-force_download = True
+force_download = False
 
 avg_predict = 0
 
-data_history_file = "bitcoin_data_4h_01012000_29042023.pkl"
+data_history_file = "bitcoin_data_4h_01012000_30042023.pkl"
 interval = Client.KLINE_INTERVAL_4HOUR
 
 while True:
@@ -78,7 +79,7 @@ while True:
         bitcoin_data.to_pickle(data_history_file)
     else:
         print("updating data to merge to existing file with data")
-        klinesT2 = Client().get_historical_klines("BTCUSDT", interval, "1 day ago UTC")
+        klinesT2 = Client(tld='us').get_historical_klines("BTCUSDT", interval, "1 day ago UTC")
         bitcoin_data2 = pd.DataFrame(klinesT2,
                                         columns=['time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av',
                                                 'trades', 'tb_base_av', 'tb_quote_av', 'ignore'])
@@ -86,10 +87,16 @@ while True:
         print("loading existing file with data")
         bitcoin_data = pd.read_pickle(data_history_file)
         print("merging downloaded recent data with existing file with data")
-        bitcoin_data = bitcoin_data.merge(bitcoin_data2, how="right")
+
+        # remove rows from bitcoin_data2 that are already in bitcoin_data
+        existing_dates = bitcoin_data['time']
+        bitcoin_data2 = bitcoin_data2[~bitcoin_data2['time'].isin(existing_dates)]
+
+        print("merging downloaded recent data with existing file with data")
+        bitcoin_data = pd.concat([bitcoin_data, bitcoin_data2], ignore_index=True, sort=False)
+
         print("saving updated data to file")
         bitcoin_data.to_pickle(data_history_file)
-
 
     # Normalisation des données d'entrée
     scaler = MinMaxScaler()
