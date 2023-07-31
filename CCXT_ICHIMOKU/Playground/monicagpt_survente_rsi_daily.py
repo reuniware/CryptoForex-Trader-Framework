20252212e30334463b1eb5cb3b8b2cc61e498c94
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-07-31 14:18:57 +0200

    Update monicagpt_survente_rsi_daily.py

--- CCXT_ICHIMOKU/Playground/monicagpt_survente_rsi_daily.py
+++ CCXT_ICHIMOKU/Playground/monicagpt_survente_rsi_daily.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import ta
 from ta.momentum import RSIIndicator
+from datetime import datetime
 
 def get_all_binance_pairs():
     url = 'https://api.binance.com/api/v3/exchangeInfo'
@@ -17,7 +18,7 @@ def get_all_binance_pairs():
 
 def get_rsi(pair):
     url = 'https://api.binance.com/api/v3/klines'
-    params = {'symbol': pair, 'interval': '1d', 'limit': 1000}
+    params = {'symbol': pair, 'interval': '4h', 'limit': 1000}
     response = requests.get(url, params=params)
     data = response.json()
     df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
@@ -34,6 +35,10 @@ for pair in pairs:
     try:
         rsi = get_rsi(pair)
         if rsi < 30:
-            print(f'{pair} est en survente RSI ({rsi:.2f})')
+            print(f'{pair} est en survente RSI ({rsi:.2f}) à {str(datetime.now()).split(".")[0]}')
+        elif rsi > 70:
+            print(f'{pair} est en surachat RSI ({rsi:.2f}) à {str(datetime.now()).split(".")[0]}')
+        #else:
+        #    print(f'{pair} RSI ({rsi:.2f})')
     except Exception as e:
         print(f'Erreur pour {pair}: {str(e)}')
