78b89be450534515beb7ca49e5c4639d299d40a7
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-07-31 23:47:02 +0200

    Update monicagpt_best_evols_t0.py

--- CCXT_ICHIMOKU/Playground/monicagpt_best_evols_t0.py
+++ CCXT_ICHIMOKU/Playground/monicagpt_best_evols_t0.py
@@ -4,6 +4,7 @@ import time
 url = "https://api.binance.com/api/v3/ticker/price"
 first_prices = {}
 previous_prices = {}
+start_time = time.time()
 
 while True:
     response = requests.get(url)
@@ -14,24 +15,27 @@ while True:
         if not first_prices:
             for price in prices:
                 symbol = price['symbol']
-                current_price = float(price['price'])
-                first_prices[symbol] = current_price
+                if symbol.endswith("USDT"):
+                    current_price = float(price['price'])
+                    first_prices[symbol] = current_price
         else:
+            current_time = time.time() - start_time
             for price in prices:
                 symbol = price['symbol']
-                current_price = float(price['price'])
-                if symbol in previous_prices:
-                    previous_price = previous_prices[symbol]
-                    price_diff = current_price - previous_price
-                    percent_change = (price_diff / first_prices[symbol]) * 100
-                    if percent_change > 0:
-                        changes[symbol] = percent_change
-                previous_prices[symbol] = current_price
+                if symbol.endswith("USDT"):
+                    current_price = float(price['price'])
+                    if symbol in previous_prices:
+                        previous_price = previous_prices[symbol]
+                        price_diff = current_price - previous_price
+                        percent_change = (price_diff / first_prices[symbol]) * 100
+                        if percent_change > 0:
+                            changes[symbol] = percent_change
+                    previous_prices[symbol] = current_price
 
             sorted_changes = sorted(changes.items(), key=lambda x: x[1], reverse=True)
             output = ""
             for symbol, change in sorted_changes[:10]:
-                output += f"{symbol}: {change:.2f}%\n"
+                output += f"{current_time:.2f}s - {symbol}: {change:.2f}%\n"
             print(output)
 
     else:
