0d00b7f45ba50077e64d4a8f84f45c3492cd4578
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2023-07-31 23:34:00 +0200

    Create monicagpt_best_evols_t0.py

---
+++ CCXT_ICHIMOKU/Playground/monicagpt_best_evols_t0.py
@@ -0,0 +1,40 @@
+import requests
+import time
+
+url = "https://api.binance.com/api/v3/ticker/price"
+first_prices = {}
+previous_prices = {}
+
+while True:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        prices = response.json()
+        changes = {}
+        if not first_prices:
+            for price in prices:
+                symbol = price['symbol']
+                current_price = float(price['price'])
+                first_prices[symbol] = current_price
+        else:
+            for price in prices:
+                symbol = price['symbol']
+                current_price = float(price['price'])
+                if symbol in previous_prices:
+                    previous_price = previous_prices[symbol]
+                    price_diff = current_price - previous_price
+                    percent_change = (price_diff / first_prices[symbol]) * 100
+                    if percent_change > 0:
+                        changes[symbol] = percent_change
+                previous_prices[symbol] = current_price
+
+            sorted_changes = sorted(changes.items(), key=lambda x: x[1], reverse=True)
+            output = ""
+            for symbol, change in sorted_changes[:10]:
+                output += f"{symbol}: {change:.2f}%\n"
+            print(output)
+
+    else:
+        print("Error retrieving prices")
+
+    time.sleep(10)
